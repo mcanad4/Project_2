010a6ff1802cbb398c4b6d7dad7f7b9ee2216a7c
Matt Canady <mcanad4@yahoo.com>
2021-07-17 13:32:32 -0400

    updated flask

--- Indy/app.py
+++ Indy/app.py
@@ -21,51 +21,94 @@ app = Flask(__name__)
 #################################################
 # Database Setup
 #################################################
-
-# Save reference to the table (update this to new table names)
-Covid = Base.classes.covidtable
-
-#################################################
 db_connection_str = os.environ.get('DATABASE_URL', '') or "postgresql://{username}:{password}@{dbhost}:{dbport}/{dbname}"
 
 engine = create_engine(db_connection_str)
 
 # reflect an existing database into a new model
 Base = automap_base()
+
 # reflect the tables
 Base.prepare(engine, reflect=True)
+
+# Save reference to the table (update this to new table names)
+Bus = Base.classes.bus_clean
+Covid = Base.classes.covid_clean
+Food = Base.classes.food_clean
+Neighborhoods = Base.classes.neighborhoods_clean
+Unemploy = Base.classes.unemploy_clean
+Zip = Base.classes.zip_clean
 
 # create route that renders index.html template
 @app.route("/")
 def home():
     return render_template("index.html")
 
+# create route that renders all the data brought in from postgres
+@app.route("/api/bus")
+def bus():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query all data in the table 
+    results = session.query(Bus).all()
+    session.close()
+    return jsonify(results)
+
+# create route that renders all the data brought in from postgres
 @app.route("/api/covid")
-def pals():
-    results = db.session.query(Pet.name, Pet.lat, Pet.lon).all()
+def covid():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
 
-    hover_text = [result[0] for result in results]
-    lat = [result[1] for result in results]
-    lon = [result[2] for result in results]
+    # Query all data in the table 
+    results = session.query(Covid).all()
+    session.close()
+    return jsonify(results)
 
-    pet_data = [{
-        "type": "scattergeo",
-        "locationmode": "USA-states",
-        "lat": lat,
-        "lon": lon,
-        "text": hover_text,
-        "hoverinfo": "text",
-        "marker": {
-            "size": 15,
-            "line": {
-                "color": "rgb(8,8,8)",
-                "width": 1
-            },
-        }
-    }]
+# create route that renders all the data brought in from postgres
+@app.route("/api/food")
+def food():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
 
-    return jsonify(pet_data)
+    # Query all data in the table 
+    results = session.query(Food).all()
+    session.close()
+    return jsonify(results)
 
+# create route that renders all the data brought in from postgres
+@app.route("/api/neighborhoods")
+def neighborhoods():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query all data in the table 
+    results = session.query(Neighborhoods).all()
+    session.close()
+    return jsonify(results)
+
+# create route that renders all the data brought in from postgres
+@app.route("/api/unemploy")
+def unemploy():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query all data in the table 
+    results = session.query(Unemploy).all()
+    session.close()
+    return jsonify(results)
+
+# create route that renders all the data brought in from postgres
+@app.route("/api/zip")
+def zip():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query all data in the table 
+    results = session.query(Zip).all()
+    session.close()
+    return jsonify(results)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
