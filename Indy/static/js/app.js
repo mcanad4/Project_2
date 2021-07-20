33e0d76e9e94e578baf86be3bc2d91aff032e38d
Lindsay Reynolds <78495351+ic17rey@users.noreply.github.com>
2021-07-20 09:48:43 -0400

    Updates for init claims plotting

--- Indy/static/js/app.js
+++ Indy/static/js/app.js
@@ -1,9 +1,35 @@
+// =====================
+// Covid Map
+// =====================
 
 
-// =============================
-// Unemployment Chart
-// =============================
+// =====================
+// Food Pantry Map
+// =====================
 
+// Create a leaflet map with Indianapolis at the center
+// set Lat and Longitude 39.76853 -86.15799
+// var indyMap = L.map('map', {
+//   center: [39.76853, -86.15799],
+//   zoom: 8
+// });
+
+// // Adding a tile layer (the background map image) to our map
+// // We use the addTo method to add objects to our map
+// L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+//   tileSize: 512,
+//   maxZoom: 18,
+//   zoomOffset: -1,
+//   id: "mapbox/streets-v11",
+//   accessToken: API_KEY
+// }).addTo(indyMap);
+
+
+
+// =====================
+// Unemployment Claims Chart
+// =====================
 
 // For live deployment, wrap unemployment chart in a function
 // function buildClaimPlot() {
@@ -37,83 +63,116 @@ var chartGroup = svg.append("g")
 // Read in the data for unemployment
 d3.csv("/assets/Clean/unemploy_clean2.csv").then(function(claimsData) {
   
-
- 
     // Parse and format the data
 
-    //index,year,month,timeframe,month_code,week,
-    //continued_claims,cont_pct_change_same_wk_last_year,init_claims,init_pct_change_same_wk_last_year    
+    //index,year,month,timeframe,month_code,week,continued_claims,
+    //cont_pct_change_same_wk_last_year,init_claims,
+    //init_pct_change_same_wk_last_year    
     
-    //var parseYearMonth = d3.timeParse("%Y-%B");
-    
-    claimsData.forEach(function(data) {
-    
-        
+    claimsData.forEach(function(data) {          
         data.index = +data.index;
         data.year = +data.year;
         data.week = +data.week;
         data.continued_claims = +data.continued_claims;
         data.init_claims = +data.init_claims;
-       
-       // These result in NaN if use the unary + operator
-       data.month = +data.month;
-       data.timeframe = data.timeframe;
-       data.month_code = +data.monthcode;
-       data.cont_pct_change_same_wk_last_year = +data.cont_pct_change_same_wk_last_year; 
-       data.init_pct_change_same_wk_last_year = +data.init_pct_change_same_wk_last_year;
-
+        data.timeframe = data.timeframe;
+        // These result in NaN if use the unary + operator
+        // data.month = +data.month;
+        // data.month_code = +data.monthcode;
+        // data.cont_pct_change_same_wk_last_year = +data.cont_pct_change_same_wk_last_year; 
+        // data.init_pct_change_same_wk_last_year = +data.init_pct_change_same_wk_last_year;
     });
 
+    // Create the area for the plot and define the x and y maximums
     var width = svgWidth - margin.left - margin.right;
     var height = svgHeight - margin.top - margin.bottom;
     
     var xMax = d3.max(claimsData, d => d.index);
     console.log(xMax);
-
-    var xLinearScale = d3.scaleLinear()
-    .domain(d3.extent(claimsData, d => d.index))
-    .range([0, width]);
-
-  
-    var yLinearScale = d3.scaleLinear().range([height, 0]);
     
     var contMax = d3.max(claimsData, d => d.continued_claims);
     var initMax = d3.max(claimsData, d => d.init_claims);
-    var yMax = contMax + initMax;
-    
-    xLinearScale.domain([0, xMax + 5]);
-    yLinearScale.domain([0, yMax + 100]);
 
-    var bottomAxis = d3.axisBottom(xLinearScale);
-    var leftAxis = d3.axisLeft(yLinearScale);
+    var yMax = 46000;
+    
+    // Create the scales and line for continued claims
+    var xLinearScaleCont = d3.scaleLinear()
+    .domain(d3.extent(claimsData, d => d.index))
+    .range([0, width]);
+
+    var yLinearScaleCont = d3.scaleLinear().range([height, 0]);
+    
+    xLinearScaleCont.domain([0, xMax + 5]);
+    yLinearScaleCont.domain([0, yMax + 20]);
+
+    var bottomAxis = d3.axisBottom(xLinearScaleCont);
+    var leftAxis = d3.axisLeft(yLinearScaleCont);
    
     chartGroup.append("g")
     .attr("transform", `translate(0, ${height})`)
     .call(bottomAxis);
 
     chartGroup.append("g").call(leftAxis);
-
-  
-        
-    var contLine = d3.line()
-    
-    .x(d => xLinearScale(d.index))
-    .y(d => yLinearScale(d.continued_claims));
+      
+    var contLine = d3.line()    
+        .x(d => xLinearScaleCont(d.index))
+        .y(d => yLinearScaleCont(d.continued_claims));
 
     chartGroup
-    .append("path")
-    .attr("d", contLine(claimsData))
-    .classed("line teal", true);
+        .append("path")
+        .attr("d", contLine(claimsData))
+        .classed("line teal", true);
 
     console.log(contMax)
     console.log(initMax)
     console.log(yMax)
     
+    // Create the initLine and the totalLine (using same x and y scale as the continued claims)
+    var initLine = d3.line()    
+        .x(d => xLinearScaleCont(d.index))
+        .y(d => yLinearScaleCont(d.init_claims));
+
+    chartGroup
+        .append("path")
+        .attr("d", initLine(claimsData))
+        .classed("line orange", true);
+
+    // var totalLine = d3.line()    
+    //     .x(d => xLinearScaleCont(d.index))
+    //     .y(d => yLinearScaleCont(d.init_claims + d.continued_claims));
+
+    // chartGroup
+    //     .append("path")
+    //     .attr("d", totalLine(claimsData))
+    //     .classed("line purple", true);
 
     // Print the data
     console.log(claimsData);
 
 });
+
+// =====================================
+// Unemployment - Initial Claims Gauge
+// =====================================
+
+// Also wrap this in the function for the time series chart
+
+//d3.json(url).then(({timeframe}) => {
+//     timeframe.forEach(period => {
+//         d3.select("select").append("option").text(period);
+//     });
+//     renderData();
+// });
+
+// Define the function that will display demographics and impact other charts
+// function renderData() {
+    // var seeMe = d3.select('select').node()
+    // console.log(seeMe)
+    
+    // Save the selected ID number to use for filtering 
+    // var sel = d3.select('select').node().value;
+    // console.log(`ID Number Selection: ${sel}`);
+    // console.log(typeof sel);
 
 
 // Create a leaflet map with Indianapolis at the center
